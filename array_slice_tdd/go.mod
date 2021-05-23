1919d1f6f70f4f63696818b1e2050fc6bb03c19e
Jimin Huh <jhuh@cloudsnap.com>
2021-05-23 00:45:39 -0500

    finished iteration, starting array and slices

---
+++ array_slice_tdd/go.mod
@@ -0,0 +1,3 @@
+module github.com/jiminhuh/golang/array_slice_tdd
+
+go 1.16
--- iteration_tdd/iteration_test.go
+++ iteration_tdd/iteration_test.go
@@ -1,14 +1,27 @@
 package iteration
 
 import (
+	"fmt"
 	"testing"
 )
 
 func TestRepeat(t *testing.T) {
-	repeated := Repeat("a")
+	repeated := Repeat("a", 5)
 	expected := "aaaaa"
 
 	if repeated != expected {
 		t.Errorf("expected %q but got %q", expected, repeated)
 	}
+}
+
+func BenchmarkRepeat(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		Repeat("a", 5)
+	}
+}
+
+func ExampleRepeat() {
+	iteratedString := Repeat("a", 5)
+	fmt.Println(iteratedString)
+	//Output: aaaaa
 }
